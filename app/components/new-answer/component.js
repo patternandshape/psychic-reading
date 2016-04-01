18d9ba849a99287bc1a3f752414c960f8e6486fd
Molly Curtin <patternandshape@gmail.com>
2016-04-01 14:26:49 -0700

    add working answer to firebase connection, now to print to page

--- app/components/new-answer/component.js
+++ app/components/new-answer/component.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newAnswer: false,
   actions: {
+    answerFormShow(){
+      this.set('newAnswer', true);
+    },
     saveAnswer1(){
       var params = {
         author: this.get('author'),
-        body: this.get('body')
+        body: this.get('body'),
+        question: this.get('question')
       };
+      this.set('newAnswer', false);
       this.sendAction('saveAnswer2', params);
     }
   }
